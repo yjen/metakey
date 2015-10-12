6858806690a0389b6efd08202d56726ccb10dfe4
yjen <yjen@berkeley.edu>
2015-10-11 18:10:53 -0700

    Fixed everything

--- metakey.js
+++ metakey.js
@@ -1,80 +1,41 @@
-Tasks = new Mongo.Collection("tasks");
- 
-if (Meteor.isServer) {
-  Meteor.publish("tasks", function () {
-    return Tasks.find();
-  }, {
-    url: "/status/:0",
-    httpMethod: "get"
-  });
-}
+Status = new Mongo.Collection("status");
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Meteor.subscribe("tasks");
-
-  Template.body.helpers({
-    tasks: function () {
-      if (Session.get("hideCompleted")) {
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
-        return Tasks.find({}, {sort: {createdAt: -1}});
-      }
-    },
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
-    },
-    incompleteCount: function () {
-      return Tasks.find({checked: {$ne: true}}).count();
+  Template.Home.events({
+    "click .circle-button": function () {
+      var id = Status.find().fetch()[0]._id;
+      Status.update(id, {
+        $set: { releasing: true, time: new Date() }
+      });
+      setTimeout(function () {
+        Status.update(id, {
+          $set: { releasing: false, time: new Date() }
+        });
+      }, 10000);
     }
-  });
-  
-  Template.body.events({
-    "submit .new-task": function (event) {
-      event.preventDefault();
-
-      var text = event.target.text.value;
-    
-      Meteor.call("addTask", text);
-      event.target.text.value = "";
-    },
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
-    }
-  });
-
-  Template.task.events({
-    "click .toggle-checked": function() {
-      Meteor.call("setChecked", this._id, ! this.checked);
-    },
-    "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
-    }
-  });
-
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
-Meteor.methods({
-  addTask: function (text) {
-    // Make sure the user is logged in before inserting a task
-    if (! Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Status.find().count() == 0) {
+      Status.insert({ releasing: false, time: new Date()});
     }
- 
-    Tasks.insert({
-      text: text,
-      createdAt: new Date(),
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
-  },
-  deleteTask: function (taskId) {
-    Tasks.remove(taskId);
-  },
-  setChecked: function (taskId, setChecked) {
-    Tasks.update(taskId, { $set: { checked: setChecked} });
-  }
+  });
+}
+
+Router.route('/', {
+  template: 'Home'
+});
+
+Router.map(function() {
+  this.route('api', {
+    path: '/api',
+    where: 'server',
+    action: function() {
+      var json = Status.find().fetch();
+      this.response.setHeader('Content-Type', 'application/json');
+      this.response.end(JSON.stringify(json));
+    }
+  });
 });
